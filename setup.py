df73daa89d85240a44ee678c8bf44f7725da3b1b
Ben Mather <brmather1@gmail.com>
2017-04-06 15:23:59 +1000

    All fortran and python signature files are kept in /src.
    srfpack and ssrfpack require tripack and stripack as dependencies --
    currently srfpack.f and ssrfpack.f are in Fortran 77 because automatic
    conversion to Fortran 90 breaks compatibility with data types in
    tripack.f90 and stripack.f90.
    
    This is a good interim approach until a better conversion can be made.

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from numpy.distutils.core  import setup, Extension
 
 # interface for Renka's algorithm 772 fortran code
-ext = Extension(name  = '_stripy',
-                sources       = ['_stripy.pyf','_stripy.f90'])
+ext1 = Extension(name    = '_stripack',
+                 sources = ['src/stripack.pyf','src/stripack.f90'])
+ext2 = Extension(name    = '_tripack',
+                 sources = ['src/tripack.pyf', 'src/tripack.f90'])
+ext3 = Extension(name    = '_srfpack',
+                 sources = ['src/srfpack.pyf', 'src/srfpack.f'])
+ext4 = Extension(name    = '_ssrfpack',
+                 sources = ['src/ssrfpack.pyf', 'src/ssrfpack.f'])
 
 if __name__ == "__main__":
     setup(name = 'stripy',
@@ -12,6 +18,6 @@ if __name__ == "__main__":
           download_url      = "",
           version           = "0.1",
           description       = "Python interface to STRIPACK fortran code for triangulation/interpolation on a sphere and a few more things",
-          ext_modules       = [ext],
+          ext_modules       = [ext1, ext2, ext3, ext4],
           packages          = ['stripy'],
           )
