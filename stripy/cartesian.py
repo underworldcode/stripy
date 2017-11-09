Fixed a number of spherical navigation, statistics and gradients
 - Vector / numpy implementation for surface distances / angles
 - Simplex containing point (lon/lat)
 - Simplices connected to a vertex, natural neighbours for a vertex
 - edge midpoints and face centroids
 - routines to subdivide mesh by triangles or lists of vertices
 -  That sort of thing …

stripy/cartesian.py
@@ -79,7 +79,7 @@ class Triangulation(object):
         The indices are 1-based (as in Fortran), not zero based (as in python).
      lend : array of ints, shape (n,)
         N pointers to adjacency lists.
-        lend(k) points to the last neighbor of node K. 
+        lend(k) points to the last neighbor of node K.
         lst(lend(K)) < 0 if and only if K is a boundary node.
         The indices are 1-based (as in Fortran), not zero based (as in python).
 
@@ -265,7 +265,7 @@ class Triangulation(object):
          yi : float / array of floats, shape (l,)
             y coordinates on the Cartesian plane
 
-        Returns 
+        Returns
         -------
          zi : float / array of floats, shape (l,)
             nearest-neighbour interpolated value(s) of (xi,yi)
@@ -310,7 +310,7 @@ class Triangulation(object):
             n = xi.size
             zi = np.empty(n)
             ist = 1
-            
+
             # iterate
             for i in range(0,n):
                 zi[i], t = _srfpack.intrc0(xi[i], yi[i], self.x, self.y, zdata,\
@@ -379,7 +379,7 @@ class Triangulation(object):
             zi  = np.empty(n)
             dzx = np.empty(n)
             dzy = np.empty(n)
-            
+
             # iterate
             for i in range(0,n):
                 zi[i], dzx[i], dzy[i], ierr = _srfpack.intrc1(xi[i], yi[i], self.x, self.y, zdata,\

stripy/spherical_meshes.py
@@ -62,9 +62,9 @@ class icosahedral_mesh(spherical.sTriangulation):
         return
 
 
-class octohedral_mesh(spherical.sTriangulation):
+class octahedral_mesh(spherical.sTriangulation):
     """
-    An octohedral triangulated mesh based on the sTriangulation class
+    An octahedral triangulated mesh based on the sTriangulation class
     """
 
     def __init__(self, refinement_levels=0, include_face_points=False):
@@ -82,7 +82,7 @@ class octohedral_mesh(spherical.sTriangulation):
         vertices_lat = np.radians(vertices_LatLonDeg.T[0])
         vertices_lon = np.radians(vertices_LatLonDeg.T[1])
 
-        super(octohedral_mesh, self).__init__(lons=vertices_lon, lats=vertices_lat)
+        super(octahedral_mesh, self).__init__(lons=vertices_lon, lats=vertices_lat)
 
         if include_face_points:
             lons, lats = self.uniformly_refine_triangulation(faces=True)
@@ -146,7 +146,6 @@ class triangulated_soccerball_mesh(spherical.sTriangulation):
         ## Now randomise the point order
         np.random.shuffle(ll)
 
-
         super(triangulated_soccerball_mesh, self).__init__(lons=ll[:,0], lats=ll[:,1])
 
         for r in range(0,refinement_levels):
@@ -155,8 +154,31 @@ class triangulated_soccerball_mesh(spherical.sTriangulation):
 
         return
 
+class random_mesh(spherical.sTriangulation):
+    """
+    A mesh of random points. Take care if you use this is parallel
+    as the location of points will not be the same on all processors
+    """
+
+    def __init__(self, number_of_points=5000):
+
+        xyz =  np.random.random((number_of_points,3)) * 2.0 - 1.0
+        xyz /= np.sqrt(xyz[:,0]**2 + xyz[:,1]**2 + xyz[:,2]**2).reshape(-1,1)
+
+        lon,lat = spherical.xyz2lonlat(xyz[:,0], xyz[:,1], xyz[:,2])
+
+        super(random_mesh, self).__init__(lons=lon, lats=lat)
+
+        return
+
 
 class uniform_ring_mesh(spherical.sTriangulation):
+    """
+    A mesh of made of rings to create a roughly gridded, even spacing on
+    the sphere. There is a small random component to prevent points lying along the
+    prime meridian so this mesh should be used with caution in parallel
+    """
+
 
     def __init__(self, resolution=9, refinement_levels=0):
 