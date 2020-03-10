e04534a1ce974c00a37db0d08026dd35ea8e270b
lmoresi <>
2020-03-10 15:41:37 +1100

    Keep cleaning up

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ import os
 import subprocess
 import sys 
 
-link_args = ""
+link_args = []
  
 if not "darwin" in sys.platform:
-    link_args = "-static"
+    link_args = ["-static"]
 
 # in development set version to none and ...
 PYPI_VERSION = "1.2.0"
@@ -69,16 +69,16 @@ with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 # interface for Renka's algorithm 772 fortran code
 ext1 = Extension(name    = 'stripy._stripack',
                  sources = ['src/stripack.pyf','src/stripack.f90'],
-                 extra_link_args=[link_args])
+                 extra_link_args=link_args)
 ext2 = Extension(name    = 'stripy._tripack',
                  sources = ['src/tripack.pyf', 'src/tripack.f90'],
-                 extra_link_args=[link_args])
+                 extra_link_args=link_args)
 ext3 = Extension(name    = 'stripy._srfpack',
                  sources = ['src/srfpack.pyf', 'src/srfpack.f'],
-                 extra_link_args=[link_args])
+                 extra_link_args=link_args)
 ext4 = Extension(name    = 'stripy._ssrfpack',
                  sources = ['src/ssrfpack.pyf', 'src/ssrfpack.f'],
-                 extra_link_args=[link_args])
+                 extra_link_args=link_args)
 
 if __name__ == "__main__":
     setup(name = 'stripy',
