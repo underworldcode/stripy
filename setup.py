19fd46e18ae1353d18525b0c59d931259ed8301a
Ben Mather <brmather1@gmail.com>
2020-08-26 21:02:28 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if "Windows" in platform.system():
     link_args = ["-static"]
 
 # in development set version to none and ...
-PYPI_VERSION = "1.3"
+PYPI_VERSION = "2.0"
 
 # Return the git revision as a string (from numpy)
 
