7926e5967fd334f52c5a2d66ad77b17bac7da908
Ben Mather <brmather1@gmail.com>
2023-03-29 17:27:32 +1100

    prepare v2.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if "Darwin"  in platform.system():
 
 
 # in development set version to none and ...
-PYPI_VERSION = "2.1.0"  # Note: don't add any dashes if you want to use conda, use b1 not .b1 
+PYPI_VERSION = "2.2"  # Note: don't add any dashes if you want to use conda, use b1 not .b1 
 
 # Return the git revision as a string (from numpy)
 
@@ -122,7 +122,8 @@ if __name__ == "__main__":
                                'Programming Language :: Python :: 3.6',
                                'Programming Language :: Python :: 3.7',
                                'Programming Language :: Python :: 3.8',
-                               'Programming Language :: Python :: 3.9'
-                               'Programming Language :: Python :: 3.10'
+                               'Programming Language :: Python :: 3.9',
+                               'Programming Language :: Python :: 3.10',
+                               'Programming Language :: Python :: 3.11'
                                ]
           )
